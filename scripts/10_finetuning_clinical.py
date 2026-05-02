e6d38404956e6e8eede9d7a7d117ee4364ed12e2
Paula Guerrero <pguerrero005@ikasle.ehu.eus>
2026-05-02 10:33:13 +0200

    initial fine-tuning scripts

--- scripts/10_finetuning_clinical.py
+++ scripts/10_finetuning_clinical.py
@@ -2,5 +2,5 @@
 10_finetuning_clinical.py
 
 use the euskera clinical corpus (backtranslated-corpus/eu-clinical_backtranslated.json) to fine-tune Latxa HiTZ/Latxa-Llama-3.1-8B-Instruct
-
+we do not have catalan clinical corpus, so we will only use the backtranslated euskera clinical corpus for fine-tuning
 """
