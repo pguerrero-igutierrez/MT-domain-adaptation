8d8f15a530b34c7ae5fb2bc4e428dc140c13d753
Paula Guerrero Castelló <guerrerocastellopaula@gmail.com>
2026-05-04 17:00:13 +0200

    Add files via upload

---
+++ scripts/11_finetuning_literaryv2.py
@@ -0,0 +1,7 @@
+"""
+11_finetuning_literaryv2.py
+
+Continue fine-tuning from 08_finetuning_general.py 
+General data + literary data (backtranslated-corpus/ca-literary_trilingual.json + backtranslated-corpus/eu-literary-trilingual.jsonl)
+
+"""
