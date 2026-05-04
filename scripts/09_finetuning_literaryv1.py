77edaad8e9e77d9a5f69be219104469c77628860
Paula Guerrero Castelló <guerrerocastellopaula@gmail.com>
2026-05-04 16:59:00 +0200

    Rename finetuning_literary.py to finetuning_literaryv1.py

--- scripts/09_finetuning_literaryv1.py
+++ scripts/09_finetuning_literaryv1.py
@@ -1,5 +1,5 @@
 """
-09_finetuning_literary.py
+09_finetuning_literaryv1.py
 
 use both the literary catalan original trilingual corpus (backtranslated-corpus/ca-literary_trilingual.json) 
 and the euskera literary corpus (backtranslated-corpus/eu-literary-trilingual.jsonl/eu-literary-trilingual.jsonl) to fine-tune Latxa HiTZ/Latxa-Llama-3.1-8B-Instruct 
