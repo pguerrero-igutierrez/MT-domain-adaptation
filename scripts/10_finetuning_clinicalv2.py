815e0daf5c6954acbf85335a87012ebfa14b1606
Paula Guerrero Castelló <guerrerocastellopaula@gmail.com>
2026-05-04 16:59:20 +0200

    Rename finetuning_clinical.py to finetuning_clinicalv2.py

--- scripts/10_finetuning_clinicalv2.py
+++ scripts/10_finetuning_clinicalv2.py
@@ -1,5 +1,5 @@
 """
-10_finetuning_clinical.py
+10_finetuning_clinicalv2.py
 
 use the euskera clinical corpus (backtranslated-corpus/eu-clinical_backtranslated.json) to fine-tune Latxa HiTZ/Latxa-Llama-3.1-8B-Instruct
 we do not have catalan clinical corpus, so we will only use the backtranslated euskera clinical corpus for fine-tuning
